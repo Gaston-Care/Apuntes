c92d296bb003472b0290ff236cc77ad110662ca2
Gaston-Care <gecare@udc.edu.ar>
2024-11-19 13:54:52 -0300

    .

--- AutenticacionAutorizacion/settings.py
+++ AutenticacionAutorizacion/settings.py
@@ -85,6 +85,17 @@ DATABASES = {
     }
 }
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': env('DB_NAME'),
+#        'USER': env('DB_USER'),
+#        'PASSWORD': env('DB_PASSWORD'),
+#        'HOST': env('DB_HOST'),
+#        'PORT': env('DB_PORT'),
+#    }
+#}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
